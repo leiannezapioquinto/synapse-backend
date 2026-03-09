31f8c0547a611e248846734af841bd06cbcda2e6
Leianneza Jae Pioquinto <lei.pioquinto@gmail.com>
2026-03-09 21:02:21 +0800

    Remove email verification routes for now

--- routes/auth.php
+++ routes/auth.php
@@ -9,15 +9,12 @@ use App\Http\Controllers\Auth\PasswordController;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisterController::class, 'register']);
     Route::post('/login', [UserAuthenticationController::class, 'login']);
-    Route::post('/email/verify/send', [EmailVerificationController::class, 'sendVerificationEmail']);
-    Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
     Route::post('/password/forgot', [PasswordController::class, 'sendResetLinkEmail']);
     Route::post('/password/reset', [PasswordController::class, 'reset']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [UserAuthenticationController::class, 'logout']);
         Route::get('/session-data', [UserAuthenticationController::class, 'me']);
-        Route::post('/email/verify/resend', [EmailVerificationController::class, 'resend']);
         Route::post('/password/change', [PasswordController::class, 'change']);
     });
 });
